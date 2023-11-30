61d1ceb1d264979f932d883e8e74148f2b03799d
Yummi <jqhinno@gmail.com>
2023-11-30 17:58:20 +0900

    Update traj_following_controller.py

--- src/home_robot/home_robot/control/traj_following_controller.py
+++ src/home_robot/home_robot/control/traj_following_controller.py
@@ -13,9 +13,15 @@ from home_robot.utils.geometry import xyt_global_to_base
 
 DEFAULT_CFG_NAME = "traj_follower"
 
-
+"""
+这段代码定义了一个轨迹跟随器（TrajFollower），它是用于导航机器人的控制器。
+该控制器接受一个轨迹函数作为输入，该函数计算在给定时间点上机器人的期望状态（位置和速度）。
+然后，控制器使用反馈控制算法计算机器人的速度命令（线速度和角速度）以跟踪这个轨迹。
+"""
 class TrajFollower:
     def __init__(self, cfg: Optional["DictConfig"] = None):
+        # 加载配置，设置比例增益（kp）和积分增益（ki）。初始化用于更新轨迹的锁和轨迹缓冲区。
+
         if cfg is None:
             cfg = get_control_config(DEFAULT_CFG_NAME)
         else:
@@ -38,14 +44,29 @@ class TrajFollower:
     def update_trajectory(
         self, traj: Callable[[float], Tuple[np.ndarray, np.ndarray, bool]]
     ):
+        """
+        更新轨迹 (update_trajectory 方法):
+
+        允许外部函数或进程更新轨迹函数。
+        使用锁来确保线程安全。
+        """
         with self._traj_update_lock:
             self.traj_buffer = traj
 
     def is_done(self) -> bool:
+        """检查是否完成 (is_done 方法):
+
+检查控制器是否已完成轨迹跟踪。"""
         return self._is_done
 
     def forward(self, xyt: np.ndarray, t: float) -> Tuple[float, float]:
-        """Returns velocity control command (v, w)"""
+        """Returns velocity control command (v, w)
+        计算控制命令 (forward 方法):
+
+查询当前时间点的轨迹以获取期望状态（位置和速度）。
+如果轨迹完成，则返回零速度。
+使用反馈控制算法计算速度命令。
+        """
         # Check for trajectory updates
         if self.traj_buffer is not None:
             with self._traj_update_lock:
@@ -72,6 +93,14 @@ class TrajFollower:
     def _feedback_controller(
         self, xyt_des: np.ndarray, dxyt_des: np.ndarray, xyt_curr: np.ndarray, dt: float
     ) -> Tuple[float, float]:
+        """
+        反馈控制器 (_feedback_controller 方法):
+
+计算期望状态和当前状态之间的误差。
+使用比例-积分（PI）控制法则来计算误差导数的期望值。
+计算速度命令以实现期望的误差导数。
+
+        """
         # Compute reference input
         u_ref = np.array([np.linalg.norm(dxyt_des[:2]), dxyt_des[2]])
 
