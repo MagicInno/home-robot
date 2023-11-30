a41d92203ef58aa7e161f266d02c87cb79ba6eef
Yummi <jqhinno@gmail.com>
2023-11-30 18:24:07 +0900

    Update complete_obs_space.py

--- src/home_robot/home_robot/agent/ovmm_agent/complete_obs_space.py
+++ src/home_robot/home_robot/agent/ovmm_agent/complete_obs_space.py
@@ -7,7 +7,32 @@
 import gym.spaces as spaces
 import numpy as np
 
+"""
+获取机器人或仿真环境中所需的所有观测空间（observation space）的超集。观测空间是一个关键概念，
+它定义了机器人感知其环境的所有可能方式。在强化学习和机器人控制中，观测空间通常包括机器人通过其传感器能够观测到的所有数据。
 
+
+观测类型:
+
+is_holding：表示机器人是否抓住了某个物体。
+head_depth：代表机器人头部的深度感知。
+joint：机器人的关节状态。
+object_embedding：物体的嵌入表示，用于物体识别或分类。
+relative_resting_position：相对休息位置。
+object_segmentation、goal_recep_segmentation、receptacle_segmentation、start_recep_segmentation：用于不同目标的语义分割。
+ovmm_nav_goal_segmentation：用于导航目标的语义分割。
+robot_start_gps 和 robot_start_compass：机器人的初始位置和方向。
+start_receptacle 和 goal_receptacle：起始和目标容器。
+空间类型:
+
+使用 gym.spaces.Box 定义具有最小值、最大值、形状和数据类型的连续空间。
+这些空间可以代表真实值（如深度信息）、分类标签或二进制标志（如是否抓住物体）。
+应用:
+
+这个函数可以在机器人硬件或仿真环境中使用，为机器人或智能体提供全面的感知信息。
+通过这些观测空间，机器人可以更好地理解其环境，并据此做出决策。
+
+"""
 def get_complete_obs_space(skill_config, baseline_config):
     """
     Get superset of observation space needed for any policy.
