8611c96567418553a862dd6626cfd3c871f7f3e2
Yummi <jqhinno@gmail.com>
2023-12-01 14:37:31 +0900

    Update writer.py

--- src/home_robot/home_robot/utils/data_tools/writer.py
+++ src/home_robot/home_robot/utils/data_tools/writer.py
@@ -11,7 +11,39 @@ import numpy as np
 import home_robot.utils.data_tools.base as base
 import home_robot.utils.data_tools.image as image
 
+"""
+用来将数据写入 HDF5 文件的工具。HDF5 是一种用于存储和组织大量数据的文件格式，常用于科学计算和机器学习。这个类提供了一种方便的方法来收集、整理和保存实验或试验的数据。以下是该类的主要功能和特点：
 
+初始化 (__init__ 方法):
+
+创建 DataWriter 实例时，可以指定文件名 (filename) 和目录名 (dirname)。
+如果提供了目录名，会尝试创建该目录。
+初始化时，还会重置（reset 方法）内部用于存储数据的字典。
+重置 (reset 方法):
+
+清空存储时间相关数据 (temporal_data)、配置数据 (config_data) 和图像数据 (img_data) 的字典。
+添加数据帧 (add_frame 方法):
+
+将一帧数据（如观测值、动作等）添加到时间相关数据字典中。
+数据通过 fix_data 方法进行预处理，以确保格式正确。
+添加图像帧 (add_img_frame 方法):
+
+专门用于添加图像类型的数据帧。
+图像数据将被转换为字节格式并存储在图像数据字典中。
+添加配置 (add_config 方法):
+
+添加与实验或试验相关的配置信息，如参数设置等。
+这些信息被视为全局数据，并存储在配置数据字典中。
+数据处理 (fix_data 和 flatten_dict 方法):
+
+用于处理嵌套字典，将其转换为扁平结构，以便更容易地存储和检索。
+写入试验数据 (write_trial 方法):
+
+将收集的数据写入 HDF5 文件。
+每个试验作为 HDF5 文件中的一个“组”进行保存，包含时序数据、配置数据和图像数据。
+同时更新试验计数器，并重置当前存储的数据。
+
+"""
 class DataWriter(object):
     """This class contains tools to write out data into an hdf5 file containing many different
     trials. This is a replacement for using numpy or pickle objects since hdf5 is slightly
